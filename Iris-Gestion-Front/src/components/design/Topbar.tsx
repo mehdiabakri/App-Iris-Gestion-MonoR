91e38884f283d80d50e7fea938d4105fbaf15993
mehdiabakri <mehdiabakri@gmail.com>
2025-08-24 13:12:36 +0200

    fix: lien vers logos et manist.json

--- Iris-Gestion-Front/src/components/design/Topbar.tsx
+++ Iris-Gestion-Front/src/components/design/Topbar.tsx
@@ -39,7 +39,7 @@ import { FaUserPlus } from "react-icons/fa6";
 import { HamburgerIcon } from "@chakra-ui/icons";
 import ExportDateModal from "../modals/ExportDateModal";
 
-import logoSrc from "../../assets/logo-Iris-Gestion.png";
+import logoSrc from "../../../public/logo-Iris-Gestion.png";
 
 export default function Topbar() {
   const clients = useDisclosure();
